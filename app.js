98748e5006a309d9c832de5678c33b2a1ca786c0
Kamil Dróżdż <kamil12x@wp.pl>
2022-09-12 20:41:39 +0200

    adds second eventlistener into app.js

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ const addElement = (e, node = 'div', txt = 'txt', attr = '', value = '') => {
 	document.querySelector('.content').appendChild(element);
 };
 
+const searchElements = (e, nameElement) => {
+	e.preventDefault();
+	const infoElement = document.querySelector('.result');
+	const elements = document.querySelectorAll(nameElement);
+	if (elements.length) {
+		infoElement.innerHTML = `<p class="result__info"> W tym dokumencie znalazłem ${elements.length} elementów ${nameElement}</p>`;
+	} else {
+		infoElement.innerHTML = `<p class="result__info"> W tym dokumencie nie znalazłem  elementów ${nameElement}</p>`;
+		return;
+	}
+};
+
+const showInfo = () => {
+	console.log('funkcja showInfo ruszyła');
+};
+
+//listenery
 const addForm = document.querySelector('.form--add');
 addForm.addEventListener('submit', e =>
 	addElement(
@@ -23,3 +40,6 @@ addForm.addEventListener('submit', e =>
 		addForm.elements.value.value
 	)
 );
+
+const searchForm = document.querySelector('.form--search');
+searchForm.addEventListener('submit', e => searchElements(e, searchForm.elements['searching-element'].value));

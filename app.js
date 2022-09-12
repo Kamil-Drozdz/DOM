7fb6715331e2a4e59ec9f235a4155ce6c99827f9
Kamil Dróżdż <kamil12x@wp.pl>
2022-09-12 18:18:49 +0200

    adds an if loop and finish the first page add a DOM

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const addElement = (e) => {
-    e.preventDeflaut();
-}
+// node,element - węzeł, tekst elementu
+const addElement = (e, node = 'div', txt = 'txt', attr = '', value = '') => {
+	e.preventDefault();
+	const element = document.createElement(node);
 
-const addForm = document.querySelector('.form--add')
-addForm.addEventListener('submit', addElement)
+	if (txt) {
+		const text = document.createTextNode(txt);
+		element.appendChild(text);
+	}
+	if (attr) {
+		element.setAttribute(attr, value);
+	}
+	document.querySelector('.content').appendChild(element);
+};
+
+const addForm = document.querySelector('.form--add');
+addForm.addEventListener('submit', e =>
+	addElement(
+		e,
+		addForm.elements.node.value,
+		addForm.elements.text.value,
+		addForm.elements.attr.value,
+		addForm.elements.value.value
+	)
+);
